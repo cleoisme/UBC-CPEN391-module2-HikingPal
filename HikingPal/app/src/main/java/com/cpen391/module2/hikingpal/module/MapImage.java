dee3e3fc042ec7c476387034558d23f09b79beb6
Yueyue Zhang <forest-zhang@hotmail.com>
2017-03-16 13:15:56 -0700

    MapImage object implemented

--- HikingPal/app/src/main/java/com/cpen391/module2/hikingpal/module/MapImage.java
+++ HikingPal/app/src/main/java/com/cpen391/module2/hikingpal/module/MapImage.java
@@ -6,7 +6,52 @@ package com.cpen391.module2.hikingpal.module;
 
 public class MapImage {
 
+    private int imageId; //put the path tp the json file
+    private long myDuration;
+    private long myDistance;
+    private String myDate;
+    private int myRating;
+
     public MapImage() {
     }
 
+    public int getImageId() {
+        return imageId;
+    }
+
+    public long getMyDistance() {
+        return myDistance;
+    }
+
+    public long getMyDuration() {
+        return myDuration;
+    }
+
+    public int getMyRating() {
+        return myRating;
+    }
+
+    public String getMyDate() {
+        return myDate;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
+    public void setMyDistance(long myDistance) {
+        this.myDistance = myDistance;
+    }
+
+    public void setMyDuration(long myDuration) {
+        this.myDuration = myDuration;
+    }
+
+    public void setMyRating(int myRating) {
+        this.myRating = myRating;
+    }
+
+    public void setMyDate(String myDate) {
+        this.myDate = myDate;
+    }
 }
--- HikingPal/app/src/main/java/com/cpen391/module2/hikingpal/utility/AlertUtil.java
+++ HikingPal/app/src/main/java/com/cpen391/module2/hikingpal/utility/AlertUtil.java
@@ -4,5 +4,5 @@ package com.cpen391.module2.hikingpal.utility;
  * Created by YueyueZhang on 2017-03-12.
  */
 
-public class AlterUtil {
+public class AlertUtil {
 }
