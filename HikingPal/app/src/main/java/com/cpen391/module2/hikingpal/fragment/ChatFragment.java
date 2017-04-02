2d32ffba4fae8c8284bfaf5d16286525f636c5cc
Yueyue Zhang <forest-zhang@hotmail.com>
2017-04-01 18:07:40 -0700

    new frags added

--- HikingPal/app/src/main/java/com/cpen391/module2/hikingpal/fragment/ChatFragment.java
+++ HikingPal/app/src/main/java/com/cpen391/module2/hikingpal/fragment/ChatFragment.java
@@ -1,7 +1,15 @@
 package com.cpen391.module2.hikingpal.fragment;
 
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.cpen391.module2.hikingpal.R;
 
 /**
  * Created by YueyueZhang on 2017-04-01.
@@ -11,5 +19,11 @@ public class ChatFragment extends Fragment {
     public ChatFragment() {
     }
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        LinearLayout ll = (LinearLayout) inflater.inflate(R.layout.announcement_history, container, false);
 
+        return ll;
+    }
 }
